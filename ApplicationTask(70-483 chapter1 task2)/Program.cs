e072eedde6fd482a01491d2bd4704c976c24ad64
Sous Chili <greatdragone75@gmail.com>
2019-06-29 03:55:17 +0400

    Finish

--- ApplicationTask(70-483 chapter1 task2)/Program.cs
+++ ApplicationTask(70-483 chapter1 task2)/Program.cs
@@ -10,12 +10,68 @@ namespace ApplicationTask_70_483_chapter1_task2_
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //без проверок считаем что пользователь не идиот
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            int stCount;
+
+            //сколько карточек студентов будет
+            Console.Write("Колличество студентов :");
+            if(!Int32.TryParse(Console.ReadLine(),out stCount))
+            {
+                Console.WriteLine("Неверный ввод по умолчанию будет 1 карточка !! ");
+                stCount = 1;
+            }
+
+            //Мутим массив 2 мерный типа string [stCount,3] в каждой строке первый элемент имя + 3 оценки
+            string[,] studCards = new string[stCount, 4];
+
+            //в цикле запрос данных о студенте
+
+            //пробегаем по студентам
+            for (int i = 0,j=0; i < stCount; i++)
+            {
+
+                Console.Write("Введите имя студента : ");
+                studCards[i, j++] = Console.ReadLine();
+                Console.WriteLine("Enter English Marks (Out Of 100) : ");
+                studCards[i,j++]= Console.ReadLine();
+                Console.WriteLine("Enter Math Marks (Out Of 100) : ");
+                studCards[i, j++] = Console.ReadLine();
+                Console.WriteLine("Enter Computer Marks (Out Of 100) : ");
+                studCards[i, j++] = Console.ReadLine();
+                j = 0; //обнуляем второй счетчик
+            }
+
+
+            //for test
+           // for (int i = 0; i < 4; i++)
+           // {
+           //     Console.WriteLine(studCards[0, i]);
+           // }
+
+
+            //Вывод карточек
+            Console.WriteLine("*****************Отчетная карта******************");
+            //Student Name: Ali Asad, Position: 1, Total: 160/300
+            for(int i=0;i<stCount;i++)
+            {
+                int rez = 0;
+                for(int j=1;j<4 ;j++)
+                {
+                    int mark;
+                    if(Int32.TryParse(studCards[i,j],out mark))
+                    {
+                        rez += mark;
+                    }
+                    else
+                    {
+                        //если ввод неверный ставим среднею оценку
+                        rez += 50;
+                    }
+                }
+                Console.WriteLine("Имя:{0}  Позиция:{1}  Оценка:{2}/300", studCards[i, 0], i, rez);
+            }
+
         }
     }
 }
